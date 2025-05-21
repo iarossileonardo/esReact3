b43f69439807b78c6c66aeb7338c01252bfa999e
lai2-24 <lai2-24@itismeucci.com>
2025-05-21 08:54:38 +0200

    modifica finita

--- app/src/Riga.js
+++ app/src/Riga.js
@@ -4,7 +4,10 @@ export default function Riga(props) {
     const alunno = props.alunno;
 
     const [conferma, setConferma] = useState();
-
+    const [mostraModifica, setMostraModifica] = useState(false);
+    const [nome, setNome] = useState("");
+    const [cognome, setCognome] = useState("");
+    
     function mostraConferma() {
         setConferma(!conferma);
     }
@@ -15,12 +18,48 @@ export default function Riga(props) {
         props.onCancella()
     }
 
+    function prepFormModifica() {
+        setMostraModifica(!mostraModifica);
+    }
+
+    async function salvaModifiche() {
+        const response = await fetch(`http://localhost:8080/alunni/${document.getElementsByName("id")[0].value}`, {method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({nome: nome, cognome: cognome})});
+        console.log(response);
+        setMostraModifica(false);
+        props.onCancella();
+    }
+
     return (
-        <tr>
-            <td>{alunno.id}</td>
-            <td>{alunno.nome}</td>
-            <td>{alunno.cognome}</td>
+        <tr>{
+        mostraModifica ? (
+                    <>
+                    <td>{alunno.id}
+                        <input name="id" id="id" value={alunno.id} type="hidden" ></input>
+                    </td>
+                    <td><input name="nome" id="nome" onChange={(event) => setNome(event.target.value)}/></td>
+                    <td><input name="cognome" id="cognome" onChange={(event) => setCognome(event.target.value)}/></td>
+                    </>) 
+                    :
+                    (<><td>{alunno.id}</td>
+                    <td>{alunno.nome}</td>
+                    <td>{alunno.cognome}</td></>)
+            }
+            
             <td>
+                {
+                !mostraModifica ? (
+                    <>
+                        <button onClick={prepFormModifica}>modifica</button>
+                    </>
+                )
+                :
+                (<>
+                    <button onClick={salvaModifiche}>Conferma</button>
+                    <button onClick={prepFormModifica}>annulla</button>
+                </>)
+                }
                 {
                     !conferma ? 
                     <button onClick={mostraConferma}>cancella</button>
